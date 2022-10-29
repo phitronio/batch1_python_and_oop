70e127e3332677886a7db889e0f094e8f98a6ba8
Programming Hero <programming.hero1@gmail.com>
2022-10-28 22:01:53 -0500

    created license authority

--- 13 OOP_Riders/user.py
+++ 13 OOP_Riders/user.py
@@ -1,5 +1,7 @@
 import hashlib
+from brta import BRTA
 
+license_authority = BRTA()
 
 class User:
     def __init__(self, name, email, password) -> None:
@@ -30,6 +32,55 @@ class User:
             return False
         # print('password found', stored_password)
 
-hero = User('Hero Alom', 'hero@alom.com', 'heroOhHero')
 
+class Rider(User):
+    def __init__(self, name, email, password, location, balance) -> None:
+        self.location = location
+        self.balance = balance
+        super().__init__(name, email, password)
+
+    def set_location(self, location):
+        self.location = location
+
+    def get_location(self):
+        return self.location
+
+    def request_trip(self, destination):
+        pass
+
+    def start_a_trip(self, fare):
+        self.balance -= fare
+
+class Driver(User):
+    def __init__(self, name, email, password, location, license) -> None:
+        super().__init__(name, email, password)
+        self.location = location
+        self.license = license
+        self.valid_driver = license_authority.validate_license(email, license)
+        self.earning = 0
+
+    def take_driving_test(self):
+        result = license_authority.take_driving_test(self.email)
+        if result == False:
+            print('Sorry you failed, try again')
+        else: 
+            self.license = result
+            self.valid_driver = True
+
+
+    def start_a_trip(self, destination, fare):
+        self.earning += fare
+        self.location = destination
+
+    
+
+hero = User('Hero Alom', 'hero@alom.com', 'heroOhHero')
 hero.log_in('hero@alom.com', 'heroOhHero')
+
+kuber = Driver('Kuber Maji', 'kuber@maji.com', 'kopilaJaisna', 54, 4556)
+
+result = license_authority.validate_license(kuber.email, kuber.license)
+print(result)
+kuber.take_driving_test()
+result = license_authority.validate_license(kuber.email, kuber.license)
+print(result)
