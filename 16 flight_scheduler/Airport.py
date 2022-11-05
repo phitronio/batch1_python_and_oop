e4c3978c8df868dad68fc9b487d33519d85a18f6
Programming Hero <programming.hero1@gmail.com>
2022-11-05 00:58:52 -0500

    calculate travel fare

--- 16 flight_scheduler/Airport.py
+++ 16 flight_scheduler/Airport.py
@@ -1,12 +1,12 @@
 class Airport:
-    def __init__(self, code, name, city, country, lat, long, rate) -> None:
+    def __init__(self, code, name, city, country, lat, lon, rate) -> None:
         self.code = code
         self.name = name
         self.city = city
         self.country = country
-        self.lat = lat
-        self.long = long
-        self.rate = rate
+        self.lat = float(lat)
+        self.lon = float(lon)
+        self.rate = float(rate)
 
     def __repr__(self) -> str:
-        return f'Airport: {self.name} in: {self.country} lat: {self.lat} long: {self.long} rate: {self.rate}'
+        return f'Airport: {self.name} in: {self.country} lat: {self.lat} long: {self.lon} rate: {self.rate}'
