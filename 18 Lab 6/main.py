06902af796fe6bea2ac79c04fd02c7827ceebc42
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-11-09 12:45:41 +0600

    lab mod 6

---
+++ 18 Lab 6/main.py
@@ -0,0 +1,33 @@
+""" object processor """
+
+import glob
+import shutil
+import os
+
+source_path = '../source/*'
+destination_path = '../destination'
+
+postfix = [1, 2, 3]
+
+while True:
+    source_object = glob.glob(source_path)
+    # print(source_object)
+
+    if len(source_object) > 0:
+
+        object_path = source_object[0]
+        shutil.copy(object_path, '.')
+
+        object_name = object_path.split('/')[-1].split('.')
+        prefix = object_name[0]
+        postfix2 = object_name[1]
+        # print(object_name)
+        # print(type(object_name))
+
+        for item in range(len(postfix)):
+            filename = prefix + '_' + str(item) + '.' + postfix2
+            print(filename)
+            shutil.copy(object_path, f"{destination_path}/{filename}")
+
+        os.remove(object_path)
+        os.remove(object_path.split('/')[-1])
